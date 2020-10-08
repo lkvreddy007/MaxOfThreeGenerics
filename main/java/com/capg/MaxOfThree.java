3295a47710c2b3f617a8cb2b67cd942ed9c829ee
lkvreddy007 <44741736+lkvreddy007@users.noreply.github.com>
2020-10-08 17:44:48 +0530

    Update MaxOfThree.java

--- main/java/com/capg/MaxOfThree.java
+++ main/java/com/capg/MaxOfThree.java
@@ -14,18 +14,6 @@ public class MaxOfThree <E extends Comparable<E>> {
 		return max;
 	}
 	
-	public static String findMax(String x,String y,String z) {
-		String max=x;
-		if(y.compareTo(max)>0) {
-			max=y;
-		}
-		if(z.compareTo(max)>0) {
-			max=z;
-		}
-		System.out.println(max);
-		return max;
-	}
-	
 	public static void main(String[] args) {
 		System.out.println("Welcome To MaxFinder");
 		findMax(1, 2, 3);
