0b1811bed60bafbc5af66fd792b70854c18e58c5
lkvreddy007 <lkvreddy007@gmail.com>
2020-10-08 18:20:00 +0530

    [Krishna]refacotered-implementedGenericsinfindMaxUsingParameterConstructor

--- test/java/com/capg/MaxOfThreeTests.java
+++ test/java/com/capg/MaxOfThreeTests.java
@@ -9,55 +9,55 @@ class MaxOfThreeTests {
 
 	@Test
 	public void givenMaxIntegerInFirstPos_WhenProper_ShouldReturnTrue() {
-		Integer n=new MaxOfThree().findMax(5, 2, 3);
+		Integer n=new MaxOfThree<Integer>(5, 2, 3).findMax();
 		Assert.assertEquals((Integer)5, n);
 	}
 	
 	@Test
 	public void givenMaxIntegerInSecondPos_WhenProper_ShouldReturnTrue() {
-		Integer n=new MaxOfThree().findMax(2, 5, 3);
+		Integer n=new MaxOfThree<Integer>(2, 5, 3).findMax();
 		Assert.assertEquals((Integer)5, n);
 	}
 	
 	@Test
 	public void givenMaxIntegerInThirdPos_WhenProper_ShouldReturnTrue() {
-		Integer n=new MaxOfThree().findMax(2, 3, 5);
+		Integer n=new MaxOfThree<Integer>(2, 3, 5).findMax();
 		Assert.assertEquals((Integer)5, n);
 	}
 	
 	@Test
 	public void givenMaxFloatInFirstPos_WhenProper_ShouldReturnTrue() {
-		Float n=new MaxOfThree().findMax(5.6f, 2.5f, 3.4f);
+		Float n=new MaxOfThree<Float>(5.6f, 2.5f, 3.4f).findMax();
 		Assert.assertEquals((Float)5.6f, n);
 	}
 	
 	@Test
 	public void givenMaxFloatInSecondPos_WhenProper_ShouldReturnTrue() {
-		Float n=new MaxOfThree().findMax(2.5f, 5.6f, 3.4f);
+		Float n=new MaxOfThree<Float>(2.5f, 5.6f, 3.4f).findMax();
 		Assert.assertEquals((Float)5.6f, n);
 	}
 	
 	@Test
 	public void givenMaxFloatInThirdPos_WhenProper_ShouldReturnTrue() {
-		Float n=new MaxOfThree().findMax(2.5f, 3.4f, 5.6f);
+		Float n=new MaxOfThree<Float>(2.5f, 3.4f, 5.6f).findMax();
 		Assert.assertEquals((Float)5.6f, n);
 	}
 	
 	@Test
 	public void givenMaxStringInFirstPos_WhenProper_ShouldReturnTrue() {
-		String n=new MaxOfThree().findMax("Peach", "Apple", "Banana");
+		String n=new MaxOfThree<String>("Peach", "Apple", "Banana").findMax();
 		Assert.assertEquals("Peach", n);
 	}
 	
 	@Test
 	public void givenMaxStringInSecondPos_WhenProper_ShouldReturnTrue() {
-		String n=new MaxOfThree().findMax("Apple", "Peach", "Banana");
+		String n=new MaxOfThree<String>("Apple", "Peach", "Banana").findMax();
 		Assert.assertEquals("Peach", n);
 	}
 	
 	@Test
 	public void givenMaxStringInThirdPos_WhenProper_ShouldReturnTrue() {
-		String n=new MaxOfThree().findMax("Apple", "Banana", "Peach");
+		String n=new MaxOfThree<String>("Apple", "Banana", "Peach").findMax();
 		Assert.assertEquals("Peach", n);
 	}
 }
