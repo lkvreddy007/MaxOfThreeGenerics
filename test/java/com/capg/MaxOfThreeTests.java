d594728e3e744c3d8e46312b60e71ae6c4250040
lkvreddy007 <lkvreddy007@gmail.com>
2020-10-08 17:40:41 +0530

    [Krishna]refacotered-impementedGenericsinfindMax

--- test/java/com/capg/MaxOfThreeTests.java
+++ test/java/com/capg/MaxOfThreeTests.java
@@ -42,4 +42,22 @@ class MaxOfThreeTests {
 		Float n=new MaxOfThree().findMax(2.5f, 3.4f, 5.6f);
 		Assert.assertEquals((Float)5.6f, n);
 	}
+	
+	@Test
+	public void givenMaxStringInFirstPos_WhenProper_ShouldReturnTrue() {
+		String n=new MaxOfThree().findMax("Peach", "Apple", "Banana");
+		Assert.assertEquals("Peach", n);
+	}
+	
+	@Test
+	public void givenMaxStringInSecondPos_WhenProper_ShouldReturnTrue() {
+		String n=new MaxOfThree().findMax("Apple", "Peach", "Banana");
+		Assert.assertEquals("Peach", n);
+	}
+	
+	@Test
+	public void givenMaxStringInThirdPos_WhenProper_ShouldReturnTrue() {
+		String n=new MaxOfThree().findMax("Apple", "Banana", "Peach");
+		Assert.assertEquals("Peach", n);
+	}
 }
