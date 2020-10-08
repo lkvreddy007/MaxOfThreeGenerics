7f5b2e93ffcba14bae30d693ff7144ee1339ac47
lkvreddy007 <lkvreddy007@gmail.com>
2020-10-08 16:46:44 +0530

    [Krishna]added-methodToFindMaxGiven3Integers

--- main/java/com/capg/MaxOfThree.java
+++ main/java/com/capg/MaxOfThree.java
@@ -1,7 +1,21 @@
 package com.capg;
 
 public class MaxOfThree {
+	
+	public static Integer findMax(Integer x,Integer y,Integer z) {
+		Integer max=x;
+		if(y.compareTo(max)>0) {
+			max=y;
+		}
+		if(z.compareTo(max)>0) {
+			max=z;
+		}
+		System.out.println(max);
+		return max;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome To MaxFinder");
+		findMax(1, 2, 3);
 	}
 }
